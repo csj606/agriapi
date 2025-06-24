1bd02ee6a1900105eeedef2c19117a9c999cdebd
csj606 <139289685+csj606@users.noreply.github.com>
2025-06-24 16:07:23 -0500

    Wrote basic Terraform script
    Wrote a simple Terraform script which spins up a EC2 instance running the AgriAPI service.

--- main.tf
+++ main.tf
@@ -2,30 +2,39 @@ terraform{
     required_providers {
         aws = {
             source = "hashicorp/aws"
-            version = "~> 3.27"
+            version = "~> 4.16"
         }
     }
+    required_version = ">= 1.2.0"
 }
 
 provider "aws" {
-    profile = "default"
     region = "us-east-1"
 }
 
-resource "aws_dynamodb_table" "in-season-table" {
-    name = "InSeasonTableIA"
-    billing_mode = "PROVISIONED"
-    read_capacity = 5
-    write_capacity = 5
-    hash_key = "month"
+resource "aws_instance" "agriapi" {
+    instance_type = "t2.micro"
+    key_name = "cloud-private-key"
+    ami = "ami-09e6f87a47903347c"
 
-    attribute {
-        name = "month"
-        type = "S"
+    tags ={
+        Name = "agriapi"
     }
 
-    tags = {
-        Name = "in-season-table-1"
-        Environment = "production"
+    connection {
+        type = "ssh"
+        user = "ec2-user"
+        private_key = file("~/.ssh/id_rsa")
+        host = self.public_ip
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+            "sudo yum update",
+            "sudo yum install golang-go git -y",
+            "git clone https://github.com/csj606/agriapi",
+            "cd api",
+            "nohup go run ingress.go &"
+        ]
     }
 }
